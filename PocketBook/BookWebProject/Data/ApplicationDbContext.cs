acc6173b30ec86b4f1515d78fc35c5c55d4a54d3
Svilen <svilen.ivanov01@gmail.com>
2022-12-10 23:08:50 +0200

    Added the EF and DB configuration

---
+++ PocketBook/BookWebProject/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BookWebProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookWebProject.Data {
+    public class ApplicationDbContext : DbContext {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {
+
+
+        }
+        public DbSet<Category> Categories { get; set; }
+
+
+    }
+}
--- PocketBook/BookWebProject/Models/Category.cs
+++ PocketBook/BookWebProject/Models/Category.cs
@@ -1,5 +1,18 @@
-﻿namespace BookWebProject.Models {
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookWebProject.Models {
     public class Category {
+
+        
+        public Category() {
+            
+        }
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
 
 
     }
