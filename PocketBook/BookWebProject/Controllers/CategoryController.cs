91644f5f0b5a894ad959ea1490b9284f6806675c
Svilen <svilen.ivanov01@gmail.com>
2022-12-10 23:28:02 +0200

    Added category view

---
+++ PocketBook/BookWebProject/Controllers/CategoryController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookWebProject.Controllers {
+    public class CategoryController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
