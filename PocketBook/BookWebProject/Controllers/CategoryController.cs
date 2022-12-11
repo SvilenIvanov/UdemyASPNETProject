9b2723c0258171c852734c50aee9f428c342112d
Svilen <svilen.ivanov01@gmail.com>
2022-12-11 23:09:08 +0200

    Updated the Category Index and Category Controller

--- PocketBook/BookWebProject/Controllers/CategoryController.cs
+++ PocketBook/BookWebProject/Controllers/CategoryController.cs
@@ -1,9 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookWebProject.Data;
+using BookWebProject.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookWebProject.Controllers {
     public class CategoryController : Controller {
+
+        private readonly ApplicationDbContext _dbContext;
+        public CategoryController(ApplicationDbContext dbContext) {
+            _dbContext = dbContext;
+        }
+
         public IActionResult Index() {
-            return View();
+
+            IEnumerable<Category> ieCategories = _dbContext.Categories;
+
+
+            return View(ieCategories);
         }
     }
 }
